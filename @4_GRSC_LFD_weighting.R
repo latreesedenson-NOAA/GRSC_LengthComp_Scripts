930e2a5d320d70816357cd57cbd16c45af1eb924
LaTreese Denson <latreese.denson@noaa.gov>
2026-01-16 18:30:43 -0500

    Update @4_GRSC_LFD_weighting.R

--- @4_GRSC_LFD_weighting.R
+++ @4_GRSC_LFD_weighting.R
@@ -9,13 +9,14 @@
 #   Combine all comp across years and states. Use West SID index proportions by 
 #   habitat to weight the combined comp. 
 #   Makes a lot of assumptions but realistically all that can be accomplished with available data.
-#
+#   June 2025  Notified that TX only used ROV data for abundance not VLL, all TX length data is now removed
+
 # Central Solution: 
 #   Scale the ROV and the VLL line gear to have equal weight and then reweight the
 #   combined data (ROV, BLL, VLL) using the habitat proportion data from the Central SID 
 # 
 # L. Denson April 2025
-#
+# updated Janurary 2026
 ################################################################################
 library(tidyverse)
 
@@ -37,7 +38,7 @@ comp_names = c("YEAR","ln_fish", names(full_set)[-1])
 # Read in data ####
 LFD_data = read.csv("GRSC_Data_for_LFD.csv")
 RSC_Proportions =read.csv("@GRSC_NumberTable.csv") 
-
+Alt_RSC_Proportions =read.csv("@GRSC_NumberTable_UCBNRCombo.csv") 
 
 # East - no weighting needed ####
 East_lfd_data = LFD_data%>%filter(SID=="East")
@@ -61,7 +62,7 @@ east_lfd_wide = east_lengths%>%pivot_wider(id_cols = c(YEAR,ln_fish),
                                            values_from = freq, 
                                            values_fill = list(freq = 0)) %>%
   left_join(full_set) %>%
-  select(comp_names) %>%
+  select(all_of(comp_names))%>%
   replace(is.na(.), 0) %>%
   mutate(SID = "East", Type = "Nominal")
 
@@ -72,10 +73,12 @@ east_lfd_wide = east_lengths%>%pivot_wider(id_cols = c(YEAR,ln_fish),
 # West Nominal ####
 west_n <- LFD_data%>%
   filter(SID == "West", HabitatType!="PC") %>% select(-year)%>%
+  filter(Region !="Texas",Gear!="VLL") %>%
   summarise(ln_fish = n())
 
 West_lengths = LFD_data%>%
   filter(SID == "West", HabitatType!="PC") %>% select(-year)%>%
+  filter(Region !="Texas",Gear!="VLL") %>%
   mutate(lbin = floor(FL_cm / bin_size)*bin_size)%>%
   mutate(ln_fish = n()) %>%
   group_by(lbin)%>% # by length bin in a certain habitat type
@@ -100,6 +103,7 @@ West_RSC = RSC_Proportions%>%filter(Region=="Texas"|Region =="Louisiana")%>%grou
 
 comps <-  LFD_data %>%
   filter(SID == "West", HabitatType!="PC") %>% 
+  filter(Region !="Texas",Gear!="VLL") %>%
   mutate(lbin = floor(FL_cm / bin_size)*bin_size)%>%
   group_by(HabitatType) %>%
   mutate(ln_fish_reg = n()) %>%
@@ -171,7 +175,7 @@ Central_lfd_wide = Central_lengths%>%pivot_wider(id_cols = c(YEAR,ln_fish),
 #View(Central_lfd_wide)
 
 # Central Weighted ####
-Central_RSC = RSC_Proportions%>%filter(Region=="Alabama"|Region =="Florida")%>%
+Central_RSC = RSC_Proportions%>%filter(Region=="Alabama")%>% # Alabama includes FL numbers now
   group_by(HabitatType)%>%
   summarize(RSC_nfish=sum((Number)))%>%filter(HabitatType!="Total")%>%
   mutate(prop_RSC=RSC_nfish/sum(RSC_nfish))
@@ -220,10 +224,59 @@ Central_lfdw_wide <- left_join(comps_central,comps_n) %>%
   mutate(SID = "Central", Type = "Weighted")
 
 #View(Central_lfdw_wide)
+# Central ALT Weighting ####
+Alt_Central_RSC = Alt_RSC_Proportions%>%filter(Region=="Alabama")%>% # Alabama includes FL but NR now includes FL UCB and NR
+  group_by(HabitatType)%>%
+  summarize(RSC_nfish=sum((Number)))%>%filter(HabitatType!="Total")%>%
+  mutate(prop_RSC=RSC_nfish/sum(RSC_nfish))
 
-Comps_Final = rbind(Central_lfd_wide,Central_lfdw_wide,east_lfd_wide,west_lfd_wide,west_lfdw_wide)
+alt_comps_central <-  LFD_data %>%
+  filter(SID == "Central",year == 2018, HabitatType!="UknS",HabitatType!="NS") %>% 
+  mutate(lbin = floor(FL_cm / bin_size)*bin_size)%>%
+  group_by(HabitatType) %>%
+  mutate(ln_fish_reg = n()) %>%
+  ungroup() %>%
+  group_by(HabitatType, ln_fish_reg, lbin)%>% # by length bin in a certain habitat type
+  summarise(freq = n() / unique(ln_fish_reg)) %>% # should be all length bins for that habitat type
+  ungroup() %>%
+  left_join(Alt_Central_RSC ,by="HabitatType") %>% # join the habitat proportions for the west to the long length data
+  mutate(lfdw_reg = freq*prop_RSC)
 
-write.csv(Comps_Final,"GRSC_weightedandNominal_LengthComps.csv")
+#View(comps)
+
+alt_comps_n <- alt_comps_central %>%
+  group_by(HabitatType, ln_fish_reg) %>%
+  summarise() %>%
+  ungroup()%>%
+  mutate(ln_fish = sum(ln_fish_reg))
+
+#View(comps_n)
+
+Alt_Central_lfdw_wide <- left_join(alt_comps_central,alt_comps_n) %>%
+  group_by(ln_fish_reg, lbin) %>%
+  mutate(lfdw1 = sum(lfdw_reg)) %>%
+  ungroup() %>%
+  mutate(RSC_rep=sum(lfdw1, na.rm = TRUE)) %>%  #proportion of landings represented in final weighted comps # good I think
+  ungroup() %>%
+  group_by( RSC_rep,ln_fish, lbin) %>%
+  summarise(
+    lfdw2=sum(lfdw1, na.rm=TRUE) ) %>%
+  mutate(
+    lfdw=lfdw2/RSC_rep) %>%
+  mutate(YEAR = 2018)%>%
+  pivot_wider(id_cols = c(YEAR,ln_fish), 
+              names_from = lbin, 
+              values_from = lfdw, 
+              values_fill = list(lfdw = 0)) %>%
+  left_join(full_set) %>%
+  select(comp_names) %>%
+  replace(is.na(.), 0)%>%
+  mutate(SID = "Central", Type = "Alt_Weighted")
+
+# Final Comps ####
+Comps_Final = rbind(Central_lfd_wide,Alt_Central_lfdw_wide,Central_lfdw_wide,east_lfd_wide,west_lfd_wide,west_lfdw_wide)
+
+write.csv(Comps_Final,"GRSC_weightedandNominal_LengthComps_updated2026.csv")
 
 #Plotting ####
 comps_final_long = Comps_Final%>%
@@ -235,16 +288,15 @@ SID.list =c("East", "West", "Central")
 
 for (i in 1:length(SID.list)){
  # i=3
-data = comps_final_long%>%filter(SID==SID.list[i])%>%mutate(lbin=as.numeric(lbin))
+data = comps_final_long%>%filter(SID==SID.list[i],Type!="Alt_Weighted")%>%mutate(lbin=as.numeric(lbin))
 n.dat=unique(data$ln_fish)
 ggplot(data,aes(x=lbin,y=prop,fill=Type))+
   geom_bar(stat = "identity",position = "dodge")+
   scale_x_continuous(breaks = seq(0,120,by=20),limits=c(0,120))+
   labs(x="Fork Length (cm)",y="Proportion at size",fill="Composition Type",
-       title= paste0(SID.list[i], " n = ",n.dat))+
+       title= paste0(SID.list[i], " unadjusted n = ",n.dat))+
   theme_minimal()
 
-Filename=paste0(SID.list[i],"_Final_LFD.jpg")
+Filename=paste0(SID.list[i],"_Final_LFD_updated2026.jpg")
 ggsave(Filename,width = 5, height = 2.5, dpi = 300, units = "in")
-
 }
